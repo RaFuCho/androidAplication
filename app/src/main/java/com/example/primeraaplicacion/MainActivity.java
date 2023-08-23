85f23e91cd69548f3db7c6bb126eda614f9b9183
Alumnos <Alumnos@SCL100PCLC30030.alumnos.cst>
2023-08-22 21:51:20 -0400

    Initial commit

---
+++ app/src/main/java/com/example/primeraaplicacion/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.primeraaplicacion;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickCartelera(View view){
+        Intent intent = new Intent(this, verCartelera.class);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/example/primeraaplicacion/verCartelera.java
@@ -0,0 +1,15 @@
+package com.example.primeraaplicacion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class verCartelera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout. activity_main);
+    }
+
+
+}
